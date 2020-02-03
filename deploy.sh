cfeceac15c3995ad2c1728ce5dd99c0a49b768f7
Carlos Peralta <carlos9917@gmail.com>
2020-02-03 11:30:12 +0100

    Renamed IMAGE to IMAGENAME and NAME to CONTAINERNAME Added if statement to stop container only if it is already running

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 source env.sh
-IMAGE=harpdev
-NAME=harprun # Name of the new image build on top of the parent image
+IMAGENAME=harpdev
+CONTAINERNAME=harprun # Name of the container based on IMAGENAME
 
 #Build the Image
-docker build -t $IMAGE .
+docker build -t $IMAGENAME .
 
 #Stop any previous containers with this name before running
-docker stop $NAME
-docker rm $NAME
+if [ ! "$(docker ps -q -f name=$CONTAINERNAME)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=$CONTAINERNAME)" ]; then
+        # cleanup
+        echo "Removing container $CONTAINERNAME"
+        docker rm $CONTAINERNAME
+    fi
+else    
+    docker stop $CONTAINERNAME
+    docker rm $CONTAINERNAME
+fi
+
 
 #Run the image in container
-#docker run -d --name $NAME $NAME
-docker run -dit -P --name $NAME -v $FURHOME/container-data:/data $IMAGE
+#docker run -d --name $CONTAINERNAME $CONTAINERNAME
+docker run -dit -P --name $CONTAINERNAME -v $FURHOME/container-data:/data $IMAGENAME
 
 #-v $FURHOME/container-data:/data specifies your local data to the binded volume inside the container.
 # $FURHOME/container-data is your local data (make your choice)
@@ -20,4 +29,4 @@ docker run -dit -P --name $NAME -v $FURHOME/container-data:/data $IMAGE
 # We recommend to keep the name of "/data"
 
 #Send STDOUT from container
-docker logs -f $NAME > docker.log
+docker logs -f $CONTAINERNAME > docker.log
